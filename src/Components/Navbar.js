e70725829403b9635fa54e5704d3e64da2bab369
Kang0 <kevinkangg@gmail.com>
2019-08-30 15:55:50 -0400

    add Components folder and Navbar file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render(){
+        return(
+            <p>Hi, this is a test sentence.</p>
+        )
+    }
+}
+
+export default Navbar
