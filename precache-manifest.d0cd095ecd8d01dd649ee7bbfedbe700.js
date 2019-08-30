b66a109424ab5f72eb2292fdacac1469b78bd358
Kang0 <kevinkangg@gmail.com>
2019-08-30 15:36:58 -0400

    Updates

---
+++ precache-manifest.d0cd095ecd8d01dd649ee7bbfedbe700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63118efe62e861f7bb5211a0b1185a14",
+    "url": "/kevin-portfolio/index.html"
+  },
+  {
+    "revision": "4eb9cb5b4b55b9dcb59d",
+    "url": "/kevin-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "133bf9ed8db3d5780828",
+    "url": "/kevin-portfolio/static/js/2.b7f053e8.chunk.js"
+  },
+  {
+    "revision": "4eb9cb5b4b55b9dcb59d",
+    "url": "/kevin-portfolio/static/js/main.4a69e7c8.chunk.js"
+  },
+  {
+    "revision": "7aef2e71e197b68b0763",
+    "url": "/kevin-portfolio/static/js/runtime~main.8ef74be9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kevin-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
