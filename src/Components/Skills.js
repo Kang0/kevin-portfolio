cf30d9d22cf3442d13c15e5d313a1d5ff7582ac0
Kang0 <kevinkangg@gmail.com>
2019-09-07 19:48:33 -0400

    add relevant skill images

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Grid, Icon, Container, Image } from 'semantic-ui-react';
 
 class Skills extends Component {
     render(){
+
+        const headerMargin = {
+            marginLeft: '300px'
+        }
+
+        const iconSize = {
+            fontSize: '10em'
+        }
         return(
-            <p>Skills</p>
+            <div>
+                <h1 style={headerMargin}>Skills</h1>
+                <br></br>
+                <Container>
+                    <Grid relaxed columns={5}>
+                        <Grid.Column>
+                            <Image src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Image src="https://image.flaticon.com/icons/png/512/1183/1183672.png" />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Image src="https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png" />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Image src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Ruby_on_Rails_logo.svg" />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Image src="https://upload.wikimedia.org/wikipedia/commons/3/3d/CSS.3.svg" />
+                        </Grid.Column>
+                    </Grid>
+                </Container>
+            </div>
         ) 
     }
 }
