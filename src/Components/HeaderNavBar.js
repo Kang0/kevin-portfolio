94c57745bfc9997ac4da4f153248e09523b1110a
Kang0 <kevinkangg@gmail.com>
2019-09-07 13:50:25 -0400

    add logo and navlinks to the right

--- src/Components/HeaderNavBar.js
+++ src/Components/HeaderNavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Divider, Icon, Grid, Segment } from 'semantic-ui-react'
+import { Divider, Icon, Grid, Segment, Image } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
 class HeaderNavBar extends Component {
@@ -13,8 +13,14 @@ class HeaderNavBar extends Component {
                         <Segment vertical><Icon name="facebook" size="large" /></Segment>
                         <Segment vertical><Icon name="medium" size="large" /></Segment>
                     </Grid.Column>
-                    <Grid.Column width={8}>
-
+                    <Grid.Column width={14}>
+                        <Image src="https://i.imgur.com/KkUwuQx.png" size="medium" centered />
+                    </Grid.Column>
+                    <Grid.Column width={1}>
+                        <Segment vertical><h5>Home</h5></Segment>
+                        <Segment vertical><h5>Projects</h5></Segment>
+                        <Segment vertical><h5>Skills</h5></Segment>
+                        <Segment vertical><h5>Experience</h5></Segment>
                     </Grid.Column>
                 </Grid>
                 <Divider />
