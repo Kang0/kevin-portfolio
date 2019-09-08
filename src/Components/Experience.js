12741f38c3e01435863070298703f8a7fcfefa7c
Kang0 <kevinkangg@gmail.com>
2019-09-08 01:26:15 -0400

    add experience

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import { Container, Grid, Icon, Segment } from 'semantic-ui-react'
 
 class Experience extends Component {
     render(){
+
+        const headerMargin = {
+            marginLeft: '300px'
+        }
         return(
-            <p>Experience</p>
+            <>
+                <h1 style={headerMargin}>Experience</h1>
+                <Container>
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Icon.Group size="huge">
+                                    <Icon size="big" name='circle outline' />
+                                    <Icon name="university" />
+                                </Icon.Group>
+                            </Grid.Column>
+                            <Grid.Column width={10}>
+                                <Segment>
+                                    Attended Flatiron School Web Development Program
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={3}>
+                                <p>Graduated 2019</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Icon.Group size="huge">
+                                    <Icon size="big" name='circle outline' />
+                                    <Icon name="law" />
+                                </Icon.Group>
+                            </Grid.Column>
+                            <Grid.Column width={10}>
+                                <Segment>
+                                    Worked at Fragomen, Del Rey, Bernsen & Loewy for 4 years as a Client Coordinator
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={3}>
+                                <p>2015-2019</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column width={3}>
+                                <Icon.Group size="huge">
+                                    <Icon size='big' name='circle outline' />
+                                    <Icon name="university" />
+                                </Icon.Group>
+                            </Grid.Column>
+                            <Grid.Column width={10}>
+                                <Segment>
+                                    Attended Boston College - BA in Political Science & Communications
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={3}>
+                                <p>Graduated 2014</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </>
         )
     }
 }
