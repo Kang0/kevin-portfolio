17ff79d92ebd2648345811c65e13c03eaba6b776
Kang0 <kevinkangg@gmail.com>
2019-09-07 20:39:50 -0400

    add project cards, only placeholder for now

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { Container, Card, Image } from 'semantic-ui-react';
 
 class Projects extends Component {
     render(){
 
         const headerMargin = {
-            marginLeft: '200px'
+            marginLeft: '300px'
         }
 
         return(
-            <h1 style={headerMargin}>Projects</h1>
+            <>
+                <h1 style={headerMargin}>Projects</h1>
+                <Container>
+                    <Card.Group itemsPerRow={3}>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+                            <Card.Content>
+                                <Card.Header>100 Days</Card.Header>
+                                <Card.Meta>FrontEnd - React / BackEnd - Ruby on Rails</Card.Meta>
+                                <Card.Description>
+                                    User's create a challenges that they must complete everyday for 100 days. Helps build new habits and track progress.
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+                            <Card.Content>
+                                <Card.Header>Record Your Records</Card.Header>
+                                <Card.Meta>FrontEnd & Backend - Ruby on Rails</Card.Meta>
+                                <Card.Description>
+                                    User's create a challenges that they must complete everyday for 100 days. Helps build new habits and track progress.
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                        <Card>
+                            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+                            <Card.Content>
+                                <Card.Header>CLI BBC Good Food Recipe</Card.Header>
+                                <Card.Meta>FrontEnd & Backend - Ruby on Rails</Card.Meta>
+                                <Card.Description>
+                                    User's create a challenges that they must complete everyday for 100 days. Helps build new habits and track progress.
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Card.Group>
+                </Container>
+            </>
         )
     }
 }
